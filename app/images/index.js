dd4085472a49cd3b9fdcd853b04632e1a5e2987f
Ryan F. Salerno <rfsalerno@gmail.com>
2020-03-15 17:35:43 -0400

    Move images directory

---
+++ app/images/index.js
@@ -0,0 +1,14 @@
+const images = {
+    '/images/gaycation.jpg': require('./gaycation.jpg'),
+    '/images/huangsworld.jpg': require('./huangsworld.jpg'),
+    '/images/kingoftheroad.jpg': require('./kingoftheroad.jpg'),
+    '/images/lastchancehigh.jpg': require('./lastchancehigh.jpg'),
+    '/images/loveindustries.jpg': require('./loveindustries.jpg'),
+    '/images/pilotweek.jpg': require('./pilotweek.jpg'),
+    '/images/statesofundress.jpg': require('./statesofundress.jpg'),
+    '/images/vicehbo.jpg': require('./vicehbo.jpg'),
+    '/images/woman.jpg': require('./woman.jpg'),
+    '/images/worldofsports.jpg': require('./worldofsports.jpg')
+};
+
+export default images;
